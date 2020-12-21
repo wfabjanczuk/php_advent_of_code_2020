699af729bcd5322da3ca56d40283eb969e6c9e2f
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2020-12-21 21:27:03 +0100

    Day03

---
+++ Day03/solvePartOne.php
@@ -0,0 +1,29 @@
+<?php
+
+function solvePartOne(string $filepath): int
+{
+    $trees = 0;
+    $map = [];
+
+    foreach (file($filepath) as $line) {
+        $row = str_split(trim($line));
+        $map[] = $row;
+    }
+
+    $xMax = count($map[0]);
+    $yMax = count($map);
+
+    $x = 0;
+    $y = 0;
+
+    while (++$y < $yMax) {
+        $x = ($x + 3) % $xMax;
+        $trees += $map[$y][$x] === '#'
+            ? 1
+            : 0;
+    }
+
+    return $trees;
+}
+
+echo solvePartOne('input.txt');
---
+++ Day03/solvePartTwo.php
@@ -0,0 +1,47 @@
+<?php
+
+function solvePartTwo(string $filepath): int
+{
+    $map = [];
+
+    foreach (file($filepath) as $line) {
+        $row = str_split(trim($line));
+        $map[] = $row;
+    }
+
+    $slopeVectors = [
+        [1, 1],
+        [3, 1],
+        [5, 1],
+        [7, 1],
+        [1, 2]
+    ];
+
+    $product = 1;
+    foreach ($slopeVectors as $slopeVector) {
+        $product *= solveSlope($map, $slopeVector[0], $slopeVector[1]);
+    }
+    return $product;
+}
+
+function solveSlope($map, $xStep, $yStep): int
+{
+    $trees = 0;
+
+    $xMax = count($map[0]);
+    $yMax = count($map);
+
+    $x = 0;
+    $y = 0;
+
+    while (($y += $yStep) < $yMax) {
+        $x = ($x + $xStep) % $xMax;
+        $trees += $map[$y][$x] === '#'
+            ? 1
+            : 0;
+    }
+
+    return $trees;
+}
+
+echo solvePartTwo('input.txt');
