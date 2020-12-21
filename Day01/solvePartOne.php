57e42304e205d3b98a8d38fee563de592cee3293
DESKTOP-D1PU670\wojci <wojciechfabjanczuk@gmail.com>
2020-12-21 20:05:44 +0100

    Day01

---
+++ Day01/solvePartOne.php
@@ -0,0 +1,22 @@
+<?php
+
+function solvePartOne(string $filepath): int
+{
+    $reverseDifferenceMap = [];
+
+    foreach (file($filepath) as $line) {
+        if (trim($line) === '') {
+            throw new \RuntimeException('Incorrect file format.');
+        }
+
+        $currentNumber = (int)$line;
+        if (array_key_exists($currentNumber, $reverseDifferenceMap)) {
+            return $currentNumber * $reverseDifferenceMap[$currentNumber];
+        }
+
+        $reverseDifferenceMap[2020 - $currentNumber] = $currentNumber;
+    }
+    throw new \RuntimeException('No solution.');
+}
+
+echo solvePartOne('input.txt');
---
+++ Day01/solvePartTwo.php
@@ -0,0 +1,26 @@
+<?php
+
+function solvePartTwo(string $filepath): int
+{
+    $pastNumbers = [];
+    $reverseDifferenceMap = [];
+
+    foreach (file($filepath) as $line) {
+        if (trim($line) === '') {
+            throw new \RuntimeException('Incorrect file format.');
+        }
+
+        $currentNumber = (int)$line;
+        if (array_key_exists($currentNumber, $reverseDifferenceMap)) {
+            return $currentNumber * array_product($reverseDifferenceMap[$currentNumber]);
+        }
+
+        foreach ($pastNumbers as $pastNumber) {
+            $reverseDifferenceMap[2020 - $pastNumber - $currentNumber] = [$pastNumber, $currentNumber];
+        }
+        $pastNumbers[] = $currentNumber;
+    }
+    throw new \RuntimeException('No solution.');
+}
+
+echo solvePartTwo('input.txt');
